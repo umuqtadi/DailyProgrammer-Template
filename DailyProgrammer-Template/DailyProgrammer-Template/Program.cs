099dd5bf424279481e8277d154d4df9ade428f68
Dustin Kraft <kraftdr@gmail.com>
2015-02-12 10:31:26 -0700

    initial check in

---
+++ DailyProgrammer-Template/DailyProgrammer-Template/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+///     REDDIT DAILY PROGRAMMER SOLUTION TEMPLATE 
+///                             http://www.reddit.com/r/dailyprogrammer
+///     Your Name: 
+///     Challenge Name:
+///     Challenge #: 
+///     Challenge URL: 
+///     Brief Description of Challenge:
+///
+/// 
+///
+///     What was difficult about this challenge?
+/// 
+///
+///     
+///
+///     What was easier than expected about this challenge?
+///     
+///
+///
+///
+
+namespace DailyProgrammer_Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
