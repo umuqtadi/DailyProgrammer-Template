c1773bac9ec81bdd09e579f9b973673bad857c51
Dustin Kraft <kraftdr@gmail.com>
2015-02-12 11:07:51 -0700

    ch ch checkin

--- DailyProgrammer-Template/DailyProgrammer-Template/Program.cs
+++ DailyProgrammer-Template/DailyProgrammer-Template/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework; //test classes need to have the using statement
 
 ///     REDDIT DAILY PROGRAMMER SOLUTION TEMPLATE 
 ///                             http://www.reddit.com/r/dailyprogrammer
@@ -24,6 +25,7 @@ using System.Threading.Tasks;
 ///
 ///
 ///
+///     BE SURE TO CREATE AT LEAST TWO TESTS FOR YOUR CODE IN THE TEST CLASS
 
 namespace DailyProgrammer_Template
 {
@@ -32,5 +34,36 @@ namespace DailyProgrammer_Template
         static void Main(string[] args)
         {
         }
+
+        /// <summary>
+        /// Simple function to illustrate how to use tests
+        /// </summary>
+        /// <param name="inputInteger"></param>
+        /// <returns></returns>
+        public static int MyTestFunction(int inputInteger)
+        {
+            return inputInteger;
+        }
     }
+
+
+#region " TEST CLASS "
+
+    //We need to use a Data Annotation [ ] to declare that this class is a Test class
+    [TestFixture]
+    class Test
+    {
+        //Test classes are declared with a return type of void.  Test classes also need a data annotation to mark them as a Test function
+        [Test]
+        public void MyTest()
+        {
+            //inside of the test, we can declare any variables that we'll need to test.  Typically, we will reference a function in your main program to test.
+            int result = Program.MyTestFunction(15);  // this function should return 15 if it is working correctly
+            //now we test for the result.
+            Assert.IsTrue(result == 15, "This is the message that displays if it does not pass");
+            // The format is:
+            // Assert.IsTrue(some boolean condition, "failure message");
+        }
+    }
+#endregion
 }
