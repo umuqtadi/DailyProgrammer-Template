b36417439041ef8d330a8f22f2af7ca8e990564e
Dustin Kraft <kraftdr@gmail.com>
2015-02-12 12:18:05 -0700

    changes

--- DailyProgrammer-Template/DailyProgrammer-Template/Program.cs
+++ DailyProgrammer-Template/DailyProgrammer-Template/Program.cs
@@ -26,6 +26,7 @@ using NUnit.Framework; //test classes need to have the using statement
 ///
 ///
 ///     BE SURE TO CREATE AT LEAST TWO TESTS FOR YOUR CODE IN THE TEST CLASS
+///     One test for a valid entry, one test for an invalid entry.
 
 namespace DailyProgrammer_Template
 {
@@ -55,7 +56,7 @@ namespace DailyProgrammer_Template
     {
         //Test classes are declared with a return type of void.  Test classes also need a data annotation to mark them as a Test function
         [Test]
-        public void MyTest()
+        public void MyValidTest()
         {
             //inside of the test, we can declare any variables that we'll need to test.  Typically, we will reference a function in your main program to test.
             int result = Program.MyTestFunction(15);  // this function should return 15 if it is working correctly
@@ -63,6 +64,13 @@ namespace DailyProgrammer_Template
             Assert.IsTrue(result == 15, "This is the message that displays if it does not pass");
             // The format is:
             // Assert.IsTrue(some boolean condition, "failure message");
+        }
+
+        [Test]
+        public void MyInvalidTest()
+        {
+            int result = Program.MyTestFunction(15);
+            Assert.IsFalse(result == 14);
         }
     }
 #endregion
